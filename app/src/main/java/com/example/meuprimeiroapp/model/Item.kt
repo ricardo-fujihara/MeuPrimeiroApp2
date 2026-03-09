35d1f1d54332eed67e6743b712eff8e79fe509bf
Ricardo Fujihara <ric.fujihara@gmail.com>
2026-03-09 20:16:11 -0300

    Adicionando ApiService/REtrofit Cliente e safeApiCall

---
+++ app/src/main/java/com/example/meuprimeiroapp/model/Item.kt
@@ -0,0 +1,45 @@
+package com.example.meuprimeiroapp.model
+
+/**
+ * Represents a simple item with a unique identifier.
+ *
+ * @property id The unique identifier for the item.
+ */
+data class Item (
+    val id: String,
+    val value: ItemValue
+)
+
+/**
+ * Detailed information about an item, including personal details and location.
+ *
+ * @property id The unique identifier for the item.
+ * @property name The first name of the person.
+ * @property surname The surname of the person.
+ * @property profession The profession of the person.
+ * @property imageUrl The URL for the person's image.
+ * @property age The age of the person.
+ * @property location The geographic location associated with the person.
+ */
+data class ItemValue(
+    val id: String,
+    val name: String,
+    val surname: String,
+    val profession: String,
+    val imageUrl: String,
+    val age: Int,
+    val location: ItemLocation
+)
+
+/**
+ * Geographic location details.
+ *
+ * @property name The name of the location.
+ * @property latitude The latitude coordinate of the location.
+ * @property longitude The longitude coordinate of the location.
+ */
+data class ItemLocation(
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
---
+++ app/src/main/java/com/example/meuprimeiroapp/service/RetrofirClient.kt
@@ -0,0 +1,22 @@
+package com.example.meuprimeiroapp.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofirClient {
+
+    private const val BASE_URL = "http://10.0.2.2:3000/" // Endereço para usado para acessar o localhost no celular
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+    val apiService: ApiService by lazy {
+        RetrofirClient.retrofit.create(ApiService::class.java)
+    }
+
+}
